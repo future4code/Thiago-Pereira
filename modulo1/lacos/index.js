4453061bba9a1e7626959b747e7216b512e8637c
ThiagoDaurizio <thiago_daurizio@hotmail.com>
2021-10-14 15:54:23 -0300

    exercicios de lacos e minhas lagrimas.

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,39 @@
+// Exercícios de interpretação de códigos
+//  ~Exercício 1
+//      Resposta: Irá ficar "loopando" o console até bater o valor 5.
+//          Ex: 1 (...) 2 (...) 3 (...) 4 (...) 5 (...)
+
+//  ~Exercício 2
+//      a. 10, 11, 12, 15
+//      b. Sinceramente eu não faço ideia, consegui absorver quase
+//         nada da aula de hoje... tudo dito muito rápido e força 
+//         avançado, eu sou leigo. :')
+
+//  ~Exercício 3
+//      Resposta: Eu acho que "4"
+
+//Exercícios de escrita de código
+//  ~Exercício 1 --INCOMPLETO.
+const numeroDeBichins = Number(prompt=("Quanto bichinhos de estimação você tem?"))
+
+if (numeroDeBichins === 0 ) {
+     console.log("Que pena você pode adotar um Pet!")
+} else {
+    console.log("houston we have a problem")
+// Não entendi nada de como opero Loop, conteudo bem dificil passado muito rápido. :/
+// Meu código morre aqui. É para mim operar o segundo Prompt aqui mesmo neste condicional? fora? um novo?
+}
+
+//  ~Exercício 2 --INCOMPLETO.
+valoresArray = [10, 20, 30]
+
+function valoresOriginais() {
+    console.log(valoresOriginais)
+}
+
+function valoresDivDez() {
+    console.log(valoresArray / 10)
+}
+
+valoresOriginais()
+valoresDivDez()
