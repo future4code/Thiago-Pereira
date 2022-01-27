2a425f4c4297b115655df0ae3e5e228f3846b925
ThiagoDaurizio <thiago_daurizio@hotmail.com>
2022-01-27 19:57:45 -0300

    api-rest

---
+++ modulo4/api-rest/src/data-users.ts
@@ -0,0 +1,58 @@
+
+enum ROLE {
+    ADMIN = "ADMIN",
+    NORMAL = "NORMAL"
+}
+
+export type Users = {
+    id: number,
+    name: string,
+    email: string,
+    type: string,
+    age: number
+}
+
+export let users: Users[] = [
+    {
+        id: 1,
+        name: "Alice",
+        email: "alice@email.com",
+        type: ROLE.ADMIN,
+        age: 12
+    },
+    {
+        id: 2,
+        name: "Bob",
+        email: "bob@email.com",
+        type: ROLE.NORMAL,
+        age: 36
+    },
+    {
+        id: 3,
+        name: "Coragem",
+        email: "coragem@email.com",
+        type: ROLE.NORMAL,
+        age: 21
+    },
+    {
+        id: 4,
+        name: "Dory",
+        email: "dory@email.com",
+        type: ROLE.NORMAL,
+        age: 17
+    },
+    {
+        id: 5,
+        name: "Elsa",
+        email: "elsa@email.com",
+        type: ROLE.ADMIN,
+        age: 17
+    },
+    {
+        id: 6,
+        name: "Fred",
+        email: "fred@email.com",
+        type: ROLE.ADMIN,
+        age: 60
+    }
+]
